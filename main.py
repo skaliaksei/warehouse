347e82e28bee2184e9a902b0ed9d03120f8a8c3b
Aliaksei Skakun <skaliaksei@gmail.com>
2024-10-16 22:00:46 +0300

    start templete of simple window

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+from PyQt6.QtWidgets import QApplication, QWidget
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# в скобках sys.argv если используем командную строку
+app = QApplication(sys.argv)
 
+window = QWidget()
+window.show()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+sys.exit(app.exec())
